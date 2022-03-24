417579b33a65d1a02fa14208bd720915757bc32e
dejandejic <dejood@hotmail.no>
2022-03-24 16:41:55 +0100

    d

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*---Hamburger menu---*/
+let menu = document.querySelector("#hamburger");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove('active');
+}
